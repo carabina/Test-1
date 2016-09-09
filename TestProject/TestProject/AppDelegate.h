5b231ab0cc12c05325fc0eb2c732b6d9161a3f52
MrChen123 <289659801@qq.com>
2016-09-09 15:01:04 +0800

    init

---
+++ TestProject/TestProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestProject
+//
+//  Created by 信辉科技 on 16/9/9.
+//  Copyright © 2016年 sinceretech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
